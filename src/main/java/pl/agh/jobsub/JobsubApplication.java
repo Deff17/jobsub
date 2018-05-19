02c27f48b95fb5414ace8b4379e1978e763cf2d5
David <dawid.prokopek@gmail.com>
2018-05-19 15:20:25 +0200

    Init commit

---
+++ src/main/java/pl/agh/jobsub/JobsubApplication.java
@@ -0,0 +1,12 @@
+package pl.agh.jobsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsubApplication.class, args);
+	}
+}
